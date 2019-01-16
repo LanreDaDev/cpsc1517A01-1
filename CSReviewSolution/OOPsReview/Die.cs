0454df039234f3e267610182b33b413468feff76
Don Welch <dwelch@nait.ca>
2019-01-16 14:52:29 -0700

    Complete Die class

--- CSReviewSolution/OOPsReview/Die.cs
+++ CSReviewSolution/OOPsReview/Die.cs
@@ -8,13 +8,21 @@ namespace OOPsReview
 {
     public class Die
     {
+        //use the System.Random class for my random number generator
+        //the instance will be static so all instances of Die will use
+        //     the same generator
+        //the instance of Random will be created when the first instance
+        //     of Die is created and be destoryed when the last instance
+        //     of Die is destoryed
+        public static Random _rnd = new Random();
+
         //Data Members
         // this is the physical storage area for data values
         // these are usually private
 
         private int _Sides;
         private string _Color;
-        private int _FaceValue;
+        
 
         //Properties
         // Properties are public
@@ -28,7 +36,7 @@ namespace OOPsReview
         // a property does NOT have a parameter list
 
         //Fully Implemented Property
-        public int Side
+        public int Sides
         {
             get
             {
@@ -45,6 +53,7 @@ namespace OOPsReview
                 if (value >= 6 && value <=20)
                 {
                     _Sides = value;
+                    Roll();
                 }
                 else
                 {
@@ -56,5 +65,83 @@ namespace OOPsReview
 
 
         //Auto Implemented Property
+        //there is no defined data member for this type of property
+        //instead, the system creates an internal storage area of the
+        //    property datatype and manages the area for your code.
+        //typically this type of property is commonly used when no
+        //    validation is required for the data
+        public int FaceValue { get; private set; }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+            set
+            {
+                //value.Trim() == "" test? empty string
+                //value == null  test? no string exists
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new  Exception("Color is invalid. You must have a color.");
+                }
+                else
+                {
+                    _Color = value;
+                }
+            }
+        }
+
+        //Constructors
+        //optionally
+        //if you do not include a constructor for your class then when an
+        //     instance of the class is created, the system will assign
+        //     the normal defualt values of that datatype to the appropriate
+        //     data member.
+
+        //if you declare a constructor within your class definition, then,
+        //     you are responsible for all constructors for the class.
+        //constructor are NOT called directly by the user of the class
+        //the constructor will be called when you ask the system to create
+        //     an instance of the class
+
+        //syntax public classname([list of parameters]) { coding body }
+
+        //default constructor
+        //this is similar to having a system constructor
+        //it has no parameters
+        public Die()
+        {
+            //typically this contructor will have no code, that is, use
+            //    the system defaults
+
+            //you could assign your own defualts
+            _Sides = 6;
+            Color = "White";
+            Roll();
+        }
+
+        //greedy constructor
+        //takes in values for each of your data members/ auto properties
+        //this allows the outside "user" wants to specify values at time
+        //      of creation for the instance
+        public Die(int sides, string color)
+        {
+            Sides = sides;
+            Color = color;
+            Roll();
+        }
+
+        //Behaviours (methods)
+        //a method will allow the user to 
+        //a) manipulate the data of the instance
+        //b) use the data of the instance to generate some other information
+
+        public void Roll()
+        {
+            //will generate a random value for FaceValue
+            FaceValue = _rnd.Next(1, Sides + 1);
+        }
     }
 }
